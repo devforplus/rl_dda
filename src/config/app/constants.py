f3c83ddbe153c44843e89c93a82d505e0836705d
devforplus <plus3456789@naver.com>
2025-05-07 22:23:25 +0900

    앱/컬러 설정 상수 파일 추가

---
+++ src/config/app/constants.py
@@ -0,0 +1,15 @@
+"""애플리케이션 상수 정의"""
+
+# 화면 크기
+APP_WIDTH = 256
+APP_HEIGHT = 192
+
+# 앱 이름
+APP_NAME = "VORTEXION"
+
+# 디스플레이 설정
+APP_DISPLAY_SCALE = 3
+APP_CAPTURE_SCALE = 1
+
+# FPS 설정
+APP_FPS = 60 
